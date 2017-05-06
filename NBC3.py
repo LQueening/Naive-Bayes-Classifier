70d71a4ba05af0a664bf36c7d2820df96ef0accb
liaokun <824343900@qq.com>
2017-05-06 09:45:31 +0800

    新增新闻数据 增加搜狗新闻词库

--- NBC3.py
+++ NBC3.py
@@ -1,17 +1,26 @@
 # coding: utf-8
-from __future__ import print_function, unicode_literals
 import os
 import time
 import random
 import jieba
-import jieba.analyse
-import numpy as np
-from collections import defaultdict
+import nltk
 import sklearn
 from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+# import pylab as pl
+import matplotlib.pyplot as plt
 
 
-# 文本预处理，遍历新闻文件进行分词，同时划分测试集和训练集
+def MakeWordsSet(words_file):
+    words_set = set()
+    with open(words_file, 'r') as fp:
+        for line in fp.readlines():
+            word = line.strip().decode("utf-8")
+            if len(word) > 0 and word not in words_set:  # 去重
+                words_set.add(word)
+    return words_set
+
+
 def TextProcessing(folder_path, test_size=0.2):
     folder_list = os.listdir(folder_path)
     data_list = []
@@ -20,21 +29,27 @@ def TextProcessing(folder_path, test_size=0.2):
     # 类间循环
     for folder in folder_list:
         new_folder_path = os.path.join(folder_path, folder)
-        # 输出相关路径和时间
-        print("路径 = ", new_folder_path, time.asctime((time.localtime(time.time()))))
         files = os.listdir(new_folder_path)
         # 类内循环
+        j = 1
         for file in files:
+            if j > 300:
+                break
             with open(os.path.join(new_folder_path, file), 'r') as fp:
                 raw = fp.read()
-            word_cut = jieba.cut(raw, cut_all=False)  # 精确模式，返回的结构是一个可迭代的genertor
-            word_list = list(word_cut)  # genertor转化为list，每个词unicode格式
+            # print raw
+            ## --------------------------------------------------------------------------------
+            ## jieba分词
+            word_cut = jieba.cut(raw, cut_all=False)
+            word_list = list(word_cut)
+            # print word_list
+            ## --------------------------------------------------------------------------------
             data_list.append(word_list)
-            class_list.append(folder)
+            class_list.append(folder.decode('utf-8'))
+            j += 1
 
-    # 划分训练集和测试集
-    data_class_list = list(zip(data_list, class_list))
-    # 返回随机排列后的序列，没有返回值，会直接修改data_class_list
+    ## 划分训练集和测试集
+    data_class_list = zip(data_list, class_list)
     random.shuffle(data_class_list)
     index = int(len(data_class_list) * test_size) + 1
     train_list = data_class_list[index:]
@@ -42,56 +57,84 @@ def TextProcessing(folder_path, test_size=0.2):
     train_data_list, train_class_list = zip(*train_list)
     test_data_list, test_class_list = zip(*test_list)
 
-    # 统计词频放入all_words
-    all_words = ''
+    # 统计词频放入all_words_dict
+    all_words_dict = {}
     for word_list in train_data_list:
         for word in word_list:
-            all_words += word
-    return all_words, train_data_list, test_data_list, train_class_list, test_class_list
+            if all_words_dict.has_key(word):
+                all_words_dict[word] += 1
+            else:
+                all_words_dict[word] = 1
+    # key函数利用词频进行降序排序
+    all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f: f[1], reverse=True)  # 内建函数sorted参数需为list
+    all_words_list = list(zip(*all_words_tuple_list)[0])
+
+    return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
 
 
-# 获取新闻的特征
-def getNewsFeatures(news, feature_words):
-    print('开始获取新闻特征')
+def words_dict(all_words_list, deleteN, stopwords_set=set()):
+    # 选取特征词
+    feature_words = []
+    n = 1
+    for t in range(deleteN, len(all_words_list), 1):
+        if n > 1000:  # feature_words的维度1000
+            break
+        # print all_words_list[t]
+        if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(
+                all_words_list[t]) < 5:
+            feature_words.append(all_words_list[t])
+            n += 1
+    return feature_words
+
+
+def TextFeatures(train_data_list, test_data_list, feature_words):
     def text_features(text, feature_words):
         text_words = set(text)
+        ## sklearn特征 list
         features = [1 if word in text_words else 0 for word in feature_words]
         return features
 
-    feature_result = [text_features(text, feature_words) for text in news]
-    return feature_result
+    train_feature_list = [text_features(text, feature_words) for text in train_data_list]
+    test_feature_list = [text_features(text, feature_words) for text in test_data_list]
+    return train_feature_list, test_feature_list
 
 
-# 新闻分类器
-def newsClassifier(train_data, train_type):
-    print('构建新闻分类器')
-    model = MultinomialNB()
-    model.fit(train_data, train_type)
-    return model
-
-
-# 使用分类器进行训练，同时使用训练之后的分类器对测试集进行分类，验证正确率
 def TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list):
-    print('开始训练')
-    classifier = newsClassifier(train_feature_list, train_class_list)
-    print('开始分类')
-    res = classifier.fit(train_feature_list, train_class_list)
-    print('获取结果')
-    test_accuracy = res.score(test_feature_list, test_class_list)
+    ## sklearn分类器
+    classifier = MultinomialNB().fit(train_feature_list, train_class_list)
+    # print classifier.predict(test_feature_list)
+    # for test_feature in test_feature_list:
+    #     print classifier.predict(test_feature)[0],
+    # print ''
+    test_accuracy = classifier.score(test_feature_list, test_class_list)
     return test_accuracy
 
 
 if __name__ == '__main__':
-    print('STARTING TIME : ', time.asctime((time.localtime(time.time()))))
-    # 文本预处理
+    print "start"
+    ## 文本预处理
     folder_path = './Database/SogouC/Sample'
-    all_words, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path,
-                                                                                                   test_size=0.2)
+    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path,
+                                                                                                        test_size=0.05)
+    # 生成stopwords_set
+    stopwords_file = './stopwords_cn.txt'
+    stopwords_set = MakeWordsSet(stopwords_file)
     ## 文本特征提取和分类
-    feature_words = jieba.analyse.extract_tags(all_words, 1500)
-    train_feature_list = getNewsFeatures(train_data_list, feature_words)
-    test_feature_list = getNewsFeatures(test_data_list, feature_words)
-    test_accuracy = TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list)
-    print('准确率 : ', test_accuracy * 100, '%')
-    print('ENDING TIME : ', time.asctime((time.localtime(time.time()))))
-    print("finished")
+    deleteNs = range(0, 1000, 20)
+    test_accuracy_list = []
+    for deleteN in deleteNs:
+        feature_words = words_dict(all_words_list, deleteN, stopwords_set)
+        train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
+        test_accuracy = TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list)
+        test_accuracy_list.append(test_accuracy)
+    print test_accuracy_list
+    # 结果评价
+    plt.figure()
+    plt.plot(deleteNs, test_accuracy_list)
+    plt.title('Relationship of deleteNs and test_accuracy')
+    plt.xlabel('deleteNs')
+    plt.ylabel('test_accuracy')
+    plt.show()
+    plt.savefig('result.png')
+
+    print "finished"
